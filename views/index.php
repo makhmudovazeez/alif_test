eebea14cd535f544107eebbafa34a0dbe749ba72
makhmudovazeez <azeezmakhmudov@gmail.com>
2022-12-07 00:18:18 +0500

    get all rooms list and get booked rooms list

--- views/index.php
+++ views/index.php
@@ -10,22 +10,24 @@
 
 <body>
 
-<div class="container">
+<div class="container" id="booking">
     <div class="card mt-5">
         <div class="card-header">Form for booking rooms</div>
         <div class="card-body">
-            <form id="booking-form" @submit.prevent="submit">
+            <form @submit.prevent="submit">
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="fullname">Fullname</label>
-                            <input type="text" name="fullname" class="form-control" v-model="full_name" placeholder="Fullname">
+                            <input type="text" name="fullname" class="form-control" v-model="full_name"
+                                   placeholder="Fullname">
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="email">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" v-model="email" placeholder="Email">
+                            <input type="email" class="form-control" id="email" name="email" v-model="email"
+                                   placeholder="Email">
                         </div>
                     </div>
                 </div>
@@ -33,13 +35,15 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="from_date">From date</label>
-                            <input type="datetime-local" class="form-control" id="from_date" v-model="from_date" name="from_date">
+                            <input type="datetime-local" class="form-control" id="from_date" v-model="from_date"
+                                   name="from_date">
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="to_date">To date</label>
-                            <input type="datetime-local" class="form-control" id="to_date" v-model="to_date" name="to_date">
+                            <input type="datetime-local" class="form-control" id="to_date" v-model="to_date"
+                                   name="to_date">
                         </div>
                     </div>
                 </div>
@@ -47,34 +51,59 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="rooms">Rooms</label>
-                            <select name="rooms" id="rooms" class="form-control" v-model="room">
+                            <select name="rooms" id="rooms" class="form-control" v-model="room_id">
                                 <option v-for="v in room_options" :value="v.id">{{ v.name }}</option>
                             </select>
                         </div>
                     </div>
                 </div>
-                <div class="d-flex justify-content-end">
-                    <div class="form-group">
-                        <button class="btn btn-success">Book</button>
-                    </div>
-                </div>
             </form>
         </div>
         <div class="card-footer">
-
+            <div class="d-flex justify-content-end">
+                <div class="form-group">
+                    <button class="btn btn-success">Book</button>
+                </div>
+            </div>
         </div>
     </div>
+    <template v-if="booked_rooms.length > 0">
+        <div class="card mt-4">
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Fullname</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">From Date</th>
+                        <th scope="col">To Date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="(br, index) in booked_rooms">
+                        <th scope="row">{{ index + 1 }}</th>
+                        <td>{{ br.full_name }}</td>
+                        <td>{{ br.email }}</td>
+                        <td>{{ br.from_date }}</td>
+                        <td>{{ br.to_date }}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </template>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 <script>
     new Vue({
-        el: "#booking-form",
+        el: "#booking",
         data: {
             host: window.location.host,
             full_name: null,
             email: null,
-            room: null,
+            room_id: null,
             from_date: null,
             to_date: null,
             room_options: [],
@@ -83,15 +112,40 @@
         },
         mounted() {
             const vm = this
+
+            fetch('/get-rooms')
+                .then(response => response.json())
+                .then(data => {
+                    vm.room_options = data
+                }).catch(err => console.log(err))
         },
         methods: {
             submit() {
 
+            },
+            getBookedRooms(room_id, from_date, to_date) {
+                if (!room_id || !from_date || !to_date){
+                    return
+                }
+                const vm = this
+                let url = `/get-booked-room?room_id=${room_id}&from_date=${from_date}&to_date=${to_date}`
+
+                fetch(url)
+                    .then(response => response.json())
+                    .then(data => {
+                        vm.booked_rooms = data
+                    })
             }
         },
         watch: {
-            'room' () {
-
+            'room_id'() {
+                this.getBookedRooms(this.room_id, this.from_date, this.to_date)
+            },
+            'from_date'() {
+                this.getBookedRooms(this.room_id, this.from_date, this.to_date)
+            },
+            'to_date'() {
+                this.getBookedRooms(this.room_id, this.from_date, this.to_date)
             }
         }
     })
