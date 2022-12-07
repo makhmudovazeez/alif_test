3137ad32647ef2bcf7f0f61764b8c87fb59c4379
makhmudovazeez <azeezmakhmudov@gmail.com>
2022-12-07 15:15:23 +0500

    new methods for booking model

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -8,12 +8,12 @@ class Booking extends Model
 {
     protected static $table = "booking";
 
-    public static function bookedRoom($request)
+    public static function bookedRoom($request): array
     {
         try {
-            $select = "select * from " . self::$table . " where room_id = {$request->room_id}";
+            $sql = "select * from " . self::$table . " where room_id = {$request->room_id}";
 
-            $select .= " and (
+            $sql .= " and (
                             (from_date BETWEEN '{$request->from_date}' and '{$request->to_date}')
                                 or
                             (to_date BETWEEN '{$request->from_date}' and '{$request->to_date}')
@@ -22,9 +22,47 @@ class Booking extends Model
                                 or
                             (to_date <= '{$request->to_date}' and to_date >= '{$request->to_date}')
                         )";
-            return self::connectToDB()->select($select)->get();
+            return self::connectToDB()->select($sql)->get();
         } catch (Exception $exception) {
-            return $exception->getMessage();
+            throw new Exception($exception->getMessage());
+        }
+    }
+
+    public static function create(array $data)
+    {
+        try {
+            $sql = "insert into " . self::$table . "(room_id, full_name, email, from_date, to_date, emailed) values ($data[room_id], '$data[full_name]', '$data[email]', '$data[from_date]', '$data[to_date]', false)";
+            $id = self::connectToDB()->insert($sql);
+            return self::getById($id);
+        } catch (Exception $exception) {
+            throw new Exception($exception->getMessage());
+        }
+    }
+
+    public function update(): bool
+    {
+        try {
+            if (!isset($this->id)) {
+                throw new Exception("object is empty");
+            }
+            $sql = "update " . self::$table . " set emailed = true where id = {$this->id}";
+            return self::connectToDB()->updateRow($sql);
+        } catch (Exception $exception) {
+            throw new Exception($exception->getMessage());
+        }
+    }
+
+    public static function getById(int $id)
+    {
+        try {
+            $sql = "select * from " . self::$table . " where id = $id";
+            $bookArray = self::connectToDB()->select($sql)->get();
+            if (count($bookArray) > 0) {
+                return $bookArray[0];
+            }
+            return null;
+        } catch (Exception $exception) {
+            throw new Exception($exception->getMessage());
         }
     }
 }
