c5c49f88fe59979037c3b337a72b2b41f5009b00
makhmudovazeez <azeezmakhmudov@gmail.com>
2022-12-07 15:14:40 +0500

    get room by id method

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -16,4 +16,18 @@ class Room extends Model
             return $exception->getMessage();
         }
     }
+
+    public static function getById(int $id)
+    {
+        try {
+            $sql = "select * from " . self::$table . " where id = $id";
+            $roomArray = self::connectToDB()->select($sql)->get();
+            if (count($roomArray) > 0) {
+                return $roomArray[0];
+            }
+            return null;
+        } catch (Exception $exception) {
+            throw new Exception($exception->getMessage());
+        }
+    }
 }
